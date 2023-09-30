1979863ec70aa4bed0aa8b0be522e8b2d382ef3d
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-09-30 22:46:45 +0500

    Update

---
+++ FlexInjectExample/Program.cs
@@ -0,0 +1,44 @@
+﻿using FlexInject;
+using FlexInject.Abstractions;
+using FlexInject.Attributes;
+using System;
+
+namespace FlexInjectExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var container = new FlexInjectContainer();
+            container.RegisterSingleton<ILoggerService, ConsoleLoggerService>();
+            container.Register<Application, Application>();
+
+            var app = container.Resolve<Application>();
+            app.Run();
+        }
+    }
+
+    public class Application
+    {
+        [Inject]
+        public ILoggerService LoggerService { get; set; }
+
+        public void Run()
+        {
+            LoggerService.Log("Hello from Application!");
+        }
+    }
+
+    public interface ILoggerService
+    {
+        void Log(string message);
+    }
+
+    public class ConsoleLoggerService : ILoggerService
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
